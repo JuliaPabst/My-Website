b0e7420c33c5bcf796314d2819533242924f77f9
Jules <ju.pabst@gmail.com>
2024-02-08 13:57:44 +0100

    add contact information and image of myself

--- my-app/src/Components/About.js
+++ my-app/src/Components/About.js
@@ -3,11 +3,24 @@ import React from "react";
 export default function About() {
   return (
     <div>
-      <h1>Julia Pabst</h1>
-      <p>
-        Hi, ich bin Jules! Diese Website soll zeigen, wer ich bin und was ich
-        bereits gemacht habe.
-      </p>
+      <section>
+        <h1>Julia Pabst</h1>
+        <img src="/square.jpg" alt="Julia Pabst" className="myImage"></img>
+        <p>
+          Hi, ich bin Jules! Diese Website soll zeigen, wer ich bin und was ich
+          bereits gemacht habe.
+        </p>
+      </section>
+      <section>
+        <h2>Kontakt</h2>
+        <div>
+          <a href="tel:+436601256182">+43 660 1256182</a>
+        </div>
+        <div>
+          <a href="mailto:ju.pabst@gmail.com">ju.pabst@gmail.com</a>
+        </div>
+        <div>Am Tabor 23/92, 1020 Wien</div>
+      </section>
     </div>
   );
 }
