1b1a8fd99231e0065749610d52161ad142669367
Jules <ju.pabst@gmail.com>
2024-02-08 16:11:51 +0100

    translating information on aboutEnglish page to english

--- my-app/src/Components/AboutEnglish.js
+++ my-app/src/Components/AboutEnglish.js
@@ -11,12 +11,12 @@ export default function AboutGerman() {
         <h2>Web & Mobile Development</h2>
         <img src="/square.jpg" alt="Julia Pabst" className="myImage"></img>
         <p>
-          Hi, ich bin Jules! Diese Website soll zeigen, wer ich bin und was ich
-          bereits gemacht habe.
+          Hi, I'm Jules! This website should show you who I am and what I have
+          already achieved.
         </p>
       </section>
       <section>
-        <h2>Kontakt</h2>
+        <h2>Contact information</h2>
         <div>
           <a href="tel:+436601256182">+43 660 1256182</a>
         </div>
@@ -69,127 +69,123 @@ export default function AboutGerman() {
               </div>
             </div>
             <div className="d-none d-lg-block d-xl-block">
-              <h2>Sprachen</h2>
-              <p>Deutsch: C2</p>
-              <p>Englisch: C2</p>
-              <p>Französisch: B1</p>
+              <h2>Languages</h2>
+              <p>German: C2</p>
+              <p>English: C2</p>
+              <p>French: B1</p>
             </div>
             <div className="d-none d-lg-block d-xl-block">
-              <h2>Interessen</h2>
-              <p>Singen in einer Band</p>
-              <p>Laufen & Radfahren</p>
-              <p>Science Fiction (Bücher, Filme, Serien)</p>
+              <h2>Interests</h2>
+              <p>Singing in a Band</p>
+              <p>Running & Cycling</p>
+              <p>Science Fiction (books, movies, series)</p>
             </div>
           </Col>
           <Col lg={9} xl={9}>
             <div>
-              <h2>Ausbildung</h2>
+              <h2>Education</h2>
               <div>
                 <h3>BSC Informatik</h3>
                 <p>FH Technikum Wien</p>
-                <p>Seit September 2023</p>
+                <p>since September 2023</p>
                 <p>
-                  Erlernen der Programmiersprachen C und PHP. Grundlagen des
-                  Datenmanagements mit SQL.
+                  Learning the programming languages C, PHP and the foundations
+                  of data management with SQL.
                 </p>
               </div>
               <div>
                 <h3>iOS & Swift - The Complete iOS App Development Bootcamp</h3>
                 <p>Udemy, Dr. Angela Yu</p>
-                <p>Seit Dezember 2023</p>
+                <p>since December 2023</p>
                 <p>
-                  Erlernen der Programmiersprache Swift. Anwendung von UIKit,
+                  Learning the programming language Swift, usage of UIKit,
                   SwiftUI, Networking, Firebase, CoreData, Realm.
                 </p>
               </div>
               <div>
-                <h3>Diplom Web-Development</h3>
+                <h3>Diploma Web-Development</h3>
                 <p>Codecool</p>
-                <p>Dezember 2022 - Juni 2023</p>
+                <p>December 2022 - June 2023</p>
                 <p>
-                  Erlernen der Programmiersprache JavaScript. Anwendung von
-                  GitHub und MERN-Stack.
+                  Learning the programming language JavaScript, usage of GitHub
+                  and MERN-Stack.
                 </p>
               </div>
               <div>
-                <h3>Diplom Frontend-Development</h3>
+                <h3>Diploma Frontend-Development</h3>
                 <p>SheCodes</p>
-                <p>November 2022 - Mai 2023</p>
-                <p>
-                  Erlernen von HTML und CSS. Anwendung von Networking und React.
-                </p>
+                <p>November 2022 - May 2023</p>
+                <p>Learning HTML and CSS, usage of Networking and React.</p>
               </div>
               <div>
-                <h3>BA Journalismus und Medienmanagement</h3>
+                <h3>BA Journalism und Media Management</h3>
                 <p>FH Wien der WKW</p>
-                <p>Oktober 2017 - Juli 2021</p>
+                <p>October 2017 - July 2021</p>
                 <p>
-                  Journalistische Theorien sowie moralische Grundlagen.
-                  Erstellung von multimedialen Inhalten.
+                  Journalistic theories and moral foundations. Production of
+                  multimedia content.
                 </p>
                 <p>Auslandssemester: Tampere University of Applied Sciences</p>
                 <p>Jänner 2020 - Mai 2020</p>
-                <p>Grundlagen des Contemporary Art Film makings.</p>
+                <p>Foundations of Contemporary Art Film Making.</p>
               </div>
               <div>
-                <h3>Diplom in Digitaler Fotografie und Bildbearbeitung</h3>
+                <h3>Diploma Digital Photography und Editing</h3>
                 <p>Lik Akademie für Foto und Design</p>
-                <p>September 2015 - Juli 2017</p>
+                <p>September 2015 - July 2017</p>
                 <p>
-                  Erlernen der unterschiedlichen Arten von Fotografie
-                  (Portraitfotografie, Landschaftsfotografie, Produktfotografie)
-                  und der digitalen Bildbearbeitung mit Adobe Photoshop.
+                  Learning different kinds of photography (portrait, landscape,
+                  product photography) and digital editing via Adobe Photoshop.
                 </p>
               </div>
             </div>
             <div>
-              <h2>Berufserfahrung</h2>
+              <h2>Career</h2>
               <div>
-                <h3>Freie Journalistin und Fotografin</h3>
+                <h3>Freelance Journalist and Photographer</h3>
                 <p>September 2017 - November 2022</p>
                 <p>Die Presse, trend, Brutkasten, Kanal3</p>
                 <p>
-                  Schreiben, Fotografie, Podcast Produktion, Instagram-Betreuung
-                  im Bereich Wirtschafts- und Technologiejournalismus
+                  Writing, photography, podcast production, Instagram Management
+                  in the areas of business and technology journalism. Schreiben,
                 </p>
               </div>
               <div>
-                <h3>Co-Founder und stellvertretende Chefredakteurin</h3>
-                <p>Juli 2020 - Juni 2022</p>
+                <h3>Co-Founder and Vice Editor in Chief</h3>
+                <p>July 2020 - June 2022</p>
                 <p>
                   <a href="amrand.at">amrand.at</a>
                 </p>
                 <p>
-                  Konzeption und Aufbau eines multimedialen Onlinemagazins.
-                  Redaktionsleitung und erstellen von multimedialem Content für
-                  die Website und Social Media.
+                  Creation of a multimedia online magazine. Management of
+                  editorial staff and production of multimedia content.
                 </p>
               </div>
               <div>
-                <h3>Praktika</h3>
+                <h3>Internships</h3>
                 <div>
-                  <h4>ORF-Korrespondentenbüro in Brüssel</h4>
-                  <p>Jänner 2021 - März 2021</p>
-                  <p>Recherche und Konzeption von Audiobeiträgen für OE1.</p>
+                  <h4>ORF Foreign Office in Brussels</h4>
+                  <p>January 2021 - March 2021</p>
+                  <p>Research and production of audio content for OE1.</p>
                 </div>
                 <div>
                   <h4>ORF ECO</h4>
                   <p>September 2019 & September 2020</p>
-                  <p>Recherche und Konzeption von TV-Beiträgen.</p>
+                  <p>Research and production of content for TV.</p>
                 </div>
               </div>
             </div>
             <div className="d-block d-lg-none">
-              <h2>Sprachen</h2>
-              <p>Deutsch: C2</p>
-              <p>Englisch: C2</p>
-              <p>Französisch: B1</p>
+              <h2>Languages</h2>
+              <p>German: C2</p>
+              <p>English: C2</p>
+              <p>French: B1</p>
             </div>
             <div className="d-block d-lg-none">
-              <h2>Interessen</h2>
-              <p>Singen in einer Band</p>
-              <p>Laufen & Radfahren</p>
-              <p>Science Fiction (Bücher, Filme, Serien)</p>
+              <h2>Interests</h2>
+              <p>Singing in a Band</p>
+              <p>Running & Cycling</p>
+              <p>Science Fiction (books, movies, series)</p>
             </div>
           </Col>
         </Row>
