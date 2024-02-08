235b890b16d378bd56b59e83cadb7a8f6ed3224f
Jules <ju.pabst@gmail.com>
2024-02-08 15:58:14 +0100

    add useState to App component, add ternary operator to decide if German or English Website should be shown. Add AboutEnglish file and copy content from AboutGerman file to it

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navigation from "./Components/Navigation.js";
-import About from "./Components/About.js";
+import AboutGerman from "./Components/AboutGerman.js";
+import AboutEnglish from "./Components/AboutEnglish.js";
+import { useState } from "react";
 
 function App() {
+  const { language, setLanguage } = useState("German");
   return (
     <div className="app">
       <header>
         <Navigation />
       </header>
-      <About />
+      {language == "German" ? <AboutGerman /> : <AboutEnglish />}
     </div>
   );
 }
--- my-app/src/Components/AboutEnglish.js
+++ my-app/src/Components/AboutEnglish.js
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-export default function About() {
+export default function AboutGerman() {
   return (
     <Container>
       <section>
