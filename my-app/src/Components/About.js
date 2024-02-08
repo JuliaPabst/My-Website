948fdb71e92a37aed5cc63208dd507e258a85e3a
Jules <ju.pabst@gmail.com>
2024-02-08 14:24:30 +0100

    add education and career information

--- my-app/src/Components/About.js
+++ my-app/src/Components/About.js
@@ -21,6 +21,106 @@ export default function About() {
         </div>
         <div>Am Tabor 23/92, 1020 Wien</div>
       </section>
+      <section>
+        <h1>CV</h1>
+        <div>
+          <h2>Ausbildung</h2>
+          <div>
+            <h3>BSC Informatik</h3>
+            <p>FH Technikum Wien</p>
+            <p>Seit September 2023</p>
+            <p>
+              Erlernen der Programmiersprachen C und PHP. Grundlagen des
+              Datenmanagements mit SQL.
+            </p>
+          </div>
+          <div>
+            <h3>iOS & Swift - The Complete iOS App Development Bootcamp</h3>
+            <p>Udemy, Dr. Angela Yu</p>
+            <p>Seit Dezember 2023</p>
+            <p>
+              Erlernen der Programmiersprache Swift. Anwendung von UIKit,
+              SwiftUI, Networking, Firebase, CoreData, Realm.
+            </p>
+          </div>
+          <div>
+            <h3>Diplom Web-Development</h3>
+            <p>Codecool</p>
+            <p>Dezember 2022 - Juni 2023</p>
+            <p>
+              Erlernen der Programmiersprache JavaScript. Anwendung von GitHub
+              und MERN-Stack.
+            </p>
+          </div>
+          <div>
+            <h3>Diplom Frontend-Development</h3>
+            <p>SheCodes</p>
+            <p>November 2022 - Mai 2023</p>
+            <p>
+              Erlernen von HTML und CSS. Anwendung von Networking und React.
+            </p>
+          </div>
+          <div>
+            <h3>BA Journalismus und Medienmanagement</h3>
+            <p>FH Wien der WKW</p>
+            <p>Oktober 2017 - Juli 2021</p>
+            <p>
+              Journalistische Theorien sowie moralische Grundlagen. Erstellung
+              von multimedialen Inhalten.
+            </p>
+            <p>Auslandssemester: Tampere University of Applied Sciences</p>
+            <p>Jänner 2020 - Mai 2020</p>
+            <p>Grundlagen des Contemporary Art Film makings.</p>
+          </div>
+          <div>
+            <h3>Diplom in Digitaler Fotografie und Bildbearbeitung</h3>
+            <p>Lik Akademie für Foto und Design</p>
+            <p>September 2015 - Juli 2017</p>
+            <p>
+              Erlernen der unterschiedlichen Arten von Fotografie
+              (Portraitfotografie, Landschaftsfotografie, Produktfotografie) und
+              der digitalen Bildbearbeitung mit Adobe Photoshop.
+            </p>
+          </div>
+        </div>
+        <div>
+          <h2>Berufserfahrung</h2>
+          <div>
+            <h3>Freie Journalistin und Fotografin</h3>
+            <p>September 2017 - November 2022</p>
+            <p>Die Presse, trend, Brutkasten, Kanal3</p>
+            <p>
+              Schreiben, Fotografie, Podcast Produktion, Instagram-Betreuung im
+              Bereich Wirtschafts- und Technologiejournalismus
+            </p>
+          </div>
+          <div>
+            <h3>Co-Founder und stellvertretende Chefredakteurin</h3>
+            <p>Juli 2020 - Juni 2022</p>
+            <p>
+              <a href="amrand.at">amrand.at</a>
+            </p>
+            <p>
+              Konzeption und Aufbau eines multimedialen Onlinemagazins.
+              Redaktionsleitung und erstellen von multimedialem Content für die
+              Website und Social Media.
+            </p>
+          </div>
+          <div>
+            <h3>Praktika</h3>
+            <div>
+              <h4>ORF-Korrespondentenbüro in Brüssel</h4>
+              <p>Jänner 2021 - März 2021</p>
+              <p>Recherche und Konzeption von Audiobeiträgen für OE1.</p>
+            </div>
+            <div>
+              <h4>ORF ECO</h4>
+              <p>September 2019 & September 2020</p>
+              <p>Recherche und Konzeption von TV-Beiträgen.</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
